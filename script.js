0cf24eca9d4fcc5c136a739d34dfe7816dd1c262
Frank Xavio <frank2006x@gmail.com>
2024-09-27 22:44:54 +0530

    Done it

--- script.js
+++ script.js
@@ -12,10 +12,9 @@ function getComputerchoice(){
     }
 
 }
-function gethumanchoice(){
+function gethumanchoice(y){
     
-    let y
-    // y=parseInt(prompt("Enter the choice"))
+    
     if (y===1){
         return 'rock'
 
@@ -31,36 +30,85 @@ function gethumanchoice(){
 
 let Humanscore=0
 let Computerscore=0
-let n=0
-console.log("1-rock")
-console.log("2-paper")
-console.log("3-scissor")
 
-while (n<6){
-    const h=gethumanchoice()
+let rock=document.getElementById('1');
+let paper=document.getElementById('2');
+let scissors=document.getElementById('3');
+let text=document.getElementById('comments')
+let Human=document.getElementById('human')
+let Computer=document.getElementById('computer')
+
+rock.addEventListener('click',()=>
+{playround(1)}
+)
+paper.addEventListener('click',()=>
+{playround(2)}
+)
+scissors.addEventListener('click',()=>
+{playround(3)}
+)
+
+
+
+function playround(y){
+    if (Humanscore >= 5 || Computerscore >= 5) {
+        return;  
+    }
+    const h=gethumanchoice(y)
     const c=getComputerchoice()
     console.log('me'+h)
     console.log('you'+c)
+    
+    console.log(y)
+    
     if (h==c){
         console.log("it is tie")
+        text.textContent=("It's Tie!!")
     }
     else {
         if (h=='rock'&& c=='scissor'){
+            text.textContent=' You WoN Rock beats Scissor'
             Humanscore++
+            Human.textContent=`player: ${Humanscore}`
+            
         }
         else if (h=='scissor'&& c=='paper'){
+            text.textContent=' You WoN Scissor beats paper'
             Humanscore++
+            Human.textContent=`player: ${Humanscore}`
         }
         else if (c=='rock'&& h=='paper'){
+            text.textContent=' You WoN Rock beats Scissor'
             Humanscore++
+            Human.textContent=`player: ${Humanscore}`
         }
-        else{
+        else if (h=='rock'&& c=='paper'){
+            text.textContent=' You LosT Paper beats Rock'
             Computerscore++
+            Computer.textContent=`player: ${Computerscore}`
         }
-        n++
+        else if (c=='scissor'&& h=='paper'){
+            text.textContent=' You LosT Scissor beats paper'
+            Computerscore++
+            Computer.textContent=`player: ${Computerscore}`
+        }
+        else if (c=='rock'&& h=='scissor'){
+            text.textContent=' You LosT Rock beats Scissor'
+            Computerscore++
+            Computer.textContent=`player: ${Computerscore}`
+        }
+
+        
+        }
+        if (Humanscore >= 5) {
+            text.textContent = 'You WIN the game!!';
+        } else if (Computerscore >= 5) {
+            text.textContent = 'Computer WINS the game!!';
+        }
+        
 
     }
     
     console.log('me:'+Humanscore)
     console.log('you:'+Computerscore)
-}
+
